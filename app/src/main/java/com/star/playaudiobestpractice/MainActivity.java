6eeb3b0325e1b72f39b6fc76e0a9b2a063859b20
crystalDf <chendong333@gmail.com>
2015-07-14 21:47:57 +0800

    Chapter 08 PlayAudioBestPractice

---
+++ app/src/main/java/com/star/playaudiobestpractice/MainActivity.java
@@ -0,0 +1,76 @@
+package com.star.playaudiobestpractice;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mPlayButton;
+    private Button mPauseButton;
+    private Button mStopButton;
+
+    private MediaPlayer mMediaPlayer = new MediaPlayer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPlayButton = (Button) findViewById(R.id.play);
+        mPauseButton = (Button) findViewById(R.id.pause);
+        mStopButton = (Button) findViewById(R.id.stop);
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.start();
+                }
+            }
+        });
+
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.pause();
+                }
+            }
+        });
+
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.reset();
+                    initMediaPlayer();
+                }
+            }
+        });
+
+        initMediaPlayer();
+    }
+
+    private void initMediaPlayer() {
+
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "Taylor Swift-Safe And Sound.mp3");
+            mMediaPlayer.setDataSource(file.getPath());
+            mMediaPlayer.prepare();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
